19dd623a80c90fa562d9049496fcb4b4183b6ef5
Peter Harper <peter.harper@raspberrypi.com>
2023-03-14 13:55:37 +0000

    Add PIO support
    Now uses a PIO to implement comms to wiznet
    Set PICO_USE_PIO=0 to disable and use "normal spi" again

--- examples/w5x00_lwiperf_test/w5x00_lwiperf_test.c
+++ examples/w5x00_lwiperf_test/w5x00_lwiperf_test.c
@@ -25,6 +25,7 @@
 
 #include "lwip/apps/lwiperf.h"
 #include "lwip/etharp.h"
+#include "pico/wiznet_spi_pio.h"
 
 /**
  * ----------------------------------------------------------------------------------------------------
@@ -52,6 +53,7 @@ static ip_addr_t g_ip;
 static ip_addr_t g_mask;
 static ip_addr_t g_gateway;
 static struct dhcp g_dhcp_client;
+wiznet_spi_handle_t spi_handle;
 
 /* LWIP */
 struct netif g_netif;
@@ -84,6 +86,91 @@ static void iperf_report(void *arg, enum lwiperf_report_type report_type,
     printf("Total iperf megabytes since start %d Mbytes\n", total_iperf_megabytes);
 }
 
+#ifndef PICO_USE_PIO
+#define PICO_USE_PIO 1
+#endif
+
+// miso
+#ifndef PICO_WIZNET_DATA_IN_PIN
+#define PICO_WIZNET_DATA_IN_PIN 16u
+#endif
+
+#ifndef PICO_WIZNET_CS_PIN
+#define PICO_WIZNET_CS_PIN 17u
+#endif
+
+#ifndef PICO_WIZNET_CLOCK_PIN
+#define PICO_WIZNET_CLOCK_PIN 18u
+#endif
+
+// mosi
+#ifndef PICO_WIZNET_DATA_OUT_PIN
+#define PICO_WIZNET_DATA_OUT_PIN 19u
+#endif
+
+#ifndef PICO_WIZNET_RESET_PIN
+#define PICO_WIZNET_RESET_PIN 20u
+#endif
+
+#ifndef PICO_WIZNET_IRQ_PIN
+#define PICO_WIZNET_IRQ_PIN 21u
+#endif
+
+#ifndef PICO_WIZNET_CLOCK_DIV_MAJOR
+#define PICO_WIZNET_CLOCK_DIV_MAJOR 2
+#endif
+
+#ifndef PICO_WIZNET_CLOCK_DIV_MINOR
+#define PICO_WIZNET_CLOCK_DIV_MINOR 0
+#endif
+
+static const wiznet_spi_config_t *wiznet_default_spi_config(void)
+{
+    static const wiznet_spi_config_t spi_config = {
+        .data_in_pin = PICO_WIZNET_DATA_IN_PIN,
+        .data_out_pin = PICO_WIZNET_DATA_OUT_PIN,
+        .cs_pin = PICO_WIZNET_CS_PIN,
+        .clock_pin = PICO_WIZNET_CLOCK_PIN,
+        .irq_pin = PICO_WIZNET_IRQ_PIN,
+        .reset_pin = PICO_WIZNET_RESET_PIN,
+        .clock_div_major = PICO_WIZNET_CLOCK_DIV_MAJOR,
+        .clock_div_minor = PICO_WIZNET_CLOCK_DIV_MINOR,
+    };
+    return &spi_config;
+}
+
+#ifndef PICO_WIZNET_VERSION
+#define PICO_WIZNET_VERSION 0x51
+#endif
+
+static int wiznet_pio_init(void) {
+    spi_handle = wiznet_spi_pio_open(wiznet_default_spi_config());
+    (*spi_handle)->reset(spi_handle);
+    (*spi_handle)->set_active(spi_handle);
+
+    reg_wizchip_spi_cbfunc((*spi_handle)->read_byte, (*spi_handle)->write_byte);
+    reg_wizchip_spiburst_cbfunc((*spi_handle)->read_buffer, (*spi_handle)->write_buffer);
+    reg_wizchip_cs_cbfunc((*spi_handle)->frame_start, (*spi_handle)->frame_end);
+
+    // Check the version
+    (*spi_handle)->frame_start();
+    uint8_t version = getVER();
+    (*spi_handle)->frame_end();
+
+    if (version != PICO_WIZNET_VERSION) {
+        panic("Failed to detect wiznet 0x%02x\n", version);
+        (*spi_handle)->set_inactive();
+        return -1;
+    }
+
+    // Configure buffers for fast MACRAW
+    uint8_t sn_size[_WIZCHIP_SOCK_NUM_ * 2] = {0};
+    sn_size[0] = _WIZCHIP_SOCK_NUM_ * 2;
+    sn_size[_WIZCHIP_SOCK_NUM_] = _WIZCHIP_SOCK_NUM_ * 2;
+    ctlwizchip(CW_INIT_WIZCHIP, sn_size);
+    return 0;
+}
+
 /**
  * ----------------------------------------------------------------------------------------------------
  * Main
@@ -108,17 +195,42 @@ int main()
 
     sleep_ms(1000 * 3); // wait for 3 seconds
 
+#if PICO_USE_PIO
+    printf("Using PIO to talk to wiznet\n");
+    if (wiznet_pio_init() != 0) {
+        printf("Failed to start with pio\n");
+        return -1;
+    }
+#else
+    printf("Using SPI to talk to wiznet\n");
     wizchip_spi_initialize((PLL_SYS_KHZ / 4) * 1000);
     wizchip_cris_initialize();
-
     wizchip_reset();
     wizchip_initialize();
+#endif
+
     wizchip_check();
 
     // Set ethernet chip MAC address
     setSHAR(mac);
     ctlwizchip(CW_SET_PHYCONF, &gPhyConf);
     ctlwizchip(CW_RESET_PHY, 0);
+
+#ifndef NDEBUG
+    getSHAR(mac);
+    assert((mac[0] | mac[1] | mac[2] | mac[3] | mac[4] | mac[5]) != 0);
+    printf("mac address %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2],
+            mac[3], mac[4], mac[5]);
+#endif
+
+#ifndef NDEBUG
+    // Now display the phy config
+    wiz_PhyConf conf_in = {0};
+    ctlwizchip(CW_GET_PHYCONF, &conf_in);
+    printf("phy config mode: %s\n", conf_in.mode ? "auto" : "manual");
+    printf("phy config speed: %s\n", conf_in.speed ? "100" : "10");
+    printf("phy config duplex: %s\n", conf_in.duplex ? "full" : "half");
+#endif
 
     // Initialize LWIP in NO_SYS mode
     lwip_init();
